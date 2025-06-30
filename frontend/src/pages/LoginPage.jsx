edf2da55d4aac8e50569af9480931f258fca0511
Bug-pirate <patiladitya914@gmail.com>
2025-06-30 11:49:26 +0530

    pages

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <div className=''>
+            <div>
+                <p>Sign in</p>
+            </div>
+            hello how are 
+        </div>
+    )
+
+}
+
+export default LoginPage
---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,13 @@
+// SignUpPage.jsx
+
+import React from 'react';
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <h1>Sign Up Page</h1>
+    </div>
+  );
+};
+
+export default SignUpPage; // ✅ Must be present for default import to work
