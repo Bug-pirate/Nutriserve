d0ab6046cda337173f5f3a0f6aac879895c58ffb
Bug-pirate <patiladitya914@gmail.com>
2025-07-24 09:50:53 +0530

    API KEY

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,18 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyCeOqr1GPMXFpy13N91WXxpypjZCHhOwMI",
-  authDomain: "nutriserve-ff504.firebaseapp.com",
-  projectId: "nutriserve-ff504",
-  storageBucket: "nutriserve-ff504.firebasestorage.app",
-  messagingSenderId: "239628471576",
-  appId: "1:239628471576:web:21ed331496faadbc3e226d",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export Auth
 export const auth = getAuth(app);
